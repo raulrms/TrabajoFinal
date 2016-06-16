4743237fe701a2ebffb755b5defcfdbf07c59fa1
Raúl Martínez <raulrms@yahoo.es>
2016-06-16 23:29:40 +0200

    Update TrabajoFinal.js

--- TrabajoFinal.js
+++ TrabajoFinal.js
@@ -5,6 +5,7 @@ db.trazas.distinct("id_usuario",{ app_acceso: "Gestor" })
 //Listado de todos los usuarios que han accedido a la aplicación en un rango de tiempo específico.
 db.trazas.find({'fecha_acceso':{$gte:ISODate('2015-09-17'),$lte:ISODate('2015-09-18')}},{_id:0,id_usuario:1})
 //Identificador del último usuario que ha utilizado la aplicación cliente.
+db.trazas.find({"app_acceso":"Cliente"},{_id:0,id_usuario:1}).sort({ fecha_acceso:-1 }).limit(1)
 //Listado de todas las URLs almacenadas y el número de accesos que ha tenido cada una.
 //Listado de todas las URLs con dos o más tags asociados.
 //Listado de usuarios que han accedido a URLs con una sola etiqueta. Además de mostrar el usuario, muestra también el tag asociado.
